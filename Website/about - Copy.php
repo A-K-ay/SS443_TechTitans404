61b0766a5d87f9d5fbe3c8be4212d3fff9190526
Fwaad Ahmad <fwaad.ahmad2@gmail.com>
2020-08-01 09:10:08 +0530

    test commit

---
+++ Website/about - Copy.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About - EV Eutopia</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/theme-casual1.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style>
+                   .img-container {
+                    text-align: center;
+                  display: block;
+				  width:100%;
+				  height:auto;
+                            }
+                </style>
+</head>
+
+<body>
+
+    <div class="brand"><img src="img/6.png" width="270" alt=""></div>
+    <div class="address-bar">The Perfect World Of Electric Vehicles.</div>
+
+   <!-- Navigation -->
+   <?php require_once 'nav.php';?>
+   
+   <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>TECH TITANS 404</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+				
+                  <img class="img-container"  src="img/b3.png" alt="">
+                </div>
+                <div class="col-md-6">
+                    <p>EV Eutopia is what we started when we decided to do something about the total negligence of EVs in Indian vehicle ecosystem.We got inspired to create this solution noticing the
+                       alarming rates of damage done to the environment. 27% of pollution in India is
+                        caused due to vehicles. Our world is facing issues because of decrement in
+                       non-renewable sources like fossil fuels and an increment in the level of
+                        pollution. Therefore the world has come up with a solution of replacing our
+                        traditional cars by EVs. EVs can reduce CO2 emissions by 30% and also make no
+                            use of non-renewable resources. But the main obstacle was introducing EVs in India.
+                         As of now according to stats the total EV market percentage in India is only 1%
+                       but EVs are slowly and steadily becoming the flavor of the season. It would not
+                       only make our air breathable but also would be beneficial for the Indian
+                        economy. So it was important to create a perfect ecosystem for electric
+                         vehicles to make their place in the Indian market.We decided to make something that would help bring a revolution in the vehicle ecosystem in india and help smoothen the transition from normal cars to EVs which help the Indian economy as much as it helps reduce pollution in our cities.
+</p>
+                    
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	</div>
+
+
+
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>EV EUTOPIA</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                  <img class="img-container"  src="img/1a.png" alt="">
+                </div>
+                <div class="col-md-6">
+                    <p>In our idea we provide a full comprehensive solution containing two applications - 
+					one for the user end and the other for the corporate end .Whenever a user registers or books
+					 an Electric Vehicle , the company receives the coordinates of its location which are fed
+					to the machine learning algorithm through firebase cloud computing. 
+					A stitch of two ML algorithms - K means clustering and DB scan algorithm provides the 
+					most suitable location to deploy a power station.When the cars are on road 
+					, our ML algorithm also tells if there are areas of heavy EV usage
+					and tells the company to deploy power stations there to prevent clogging.
+					For the user end it provides all the basic functionalities like unlocking 
+					and locking of car , displaying its range , etc. 
+					One of its feature is to display the battery percentage of EV 
+					, it also notifies and tells the user about the power stations available on users 
+					route whenever the battery is less than threshold.</p>
+                    
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	</div>	
+	
+		
+		 <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Key Feature Of
+                        <strong>EV EUTOPIA</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-lg-12">
+                 <img class="img-container"  src="img/b4.png" alt="">
+                </div>
+                
+                <div class="clearfix"></div>
+            </div>
+        </div>
+		</div>
+		
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">EV's aren't important,
+                        <strong>they are necessity!</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                  <img class="img-container"  src="img/b2.jpg" alt="">
+                </div>
+                <div class="col-md-6">
+                    <p>Electric vehicles are around 3-5 times more efficient than internal combustion vehicles in utilising energy. Even if electric vehicles run on electricity produced from fossil fuels, the overall efficiency of electric vehicles is still higher and the pollution is less, because large thermal power plants are much more efficient than IC engines, and it is easier to control emissions from power plants than vehicle engines.Air quality indices related to India indicate that the air in many cities of India is no longer healthy. Automobile related pollution has been one of the causes for this.
+Aspects related to global warming needs a shift to automobile solutions that reduce / do not produce greenhouse gas emissions. If electric vehicles run on electricity produced from non-polluting sources of energy like hydro, solar, wind, tidal and nuclear, they reduce emissions due to vehicles almost to zero.
+The need to reduce dependency on a fossil-fuel based economy. India's crude oil imports for 2014-15 was 112 billion dollars (approximately 7,00,000 crore rupees). For comparison, the allocation for the Mahatma Gandhi National Rural Employment Guarantee Scheme, in budget 2017-18, is 48,000 crore rupees.People living in some Indian cities are being affected by noise pollution. Some of the Indian cities have the worst noise pollution levels in the world.Electric vehicles are much quieter and may contribute to a reduction in noise pollution levels in the cities.<br>India has great expectations of achieving a high level of penetration in e-mobility by 2030. The reason is not very surprising; the alarming levels of pollution indices which keep on rising and the colossal dollars the country must pay for annual crude oil imports. In December 2017, New Delhi was in a state of red alert and came close to Beijing in terms of pollution toxicity, such are the pollution indices in India. If India successfully manages to achieve this target by 2030, it could save about 1 Giga Tonne of emissions.</p>
+                    
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	</div>	
+	
+	<div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Our
+                        <strong>Team</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-sm-2 text-center">
+                    <img class="img-responsive" src="img/14.jpg" alt="">
+                    <h3>Abhimanyu Kharwal<br>
+                        <small>ML DEVELOPER</small>
+                    </h3>
+                </div>
+                <div class="col-sm-2 text-center">
+                    <img class="img-responsive" src="img/16.jpg" alt="">
+                    <h3>Ashita Mahajan<br>
+                        <small>CONTENT MANAGER AND DEVELOPER</small>
+                    </h3>
+                </div>
+                <div class="col-sm-2 text-center">
+                    <img class="img-responsive" src="img/18.jpg" alt="">
+                    <h3>Fwaad Ahmad <br>
+                    
+						<small>APP. DEVELOPER</small>
+                    </h3>
+                </div>
+				<div class="col-sm-2 text-center">
+                    <img class="img-responsive" src="img/15.jpg" alt="">
+                     <h3>Arshiya<br>
+                        <small>DATABASE MANAGER</small>
+                    </h3>
+                </div>
+                <div class="col-sm-2 text-center">
+                    <img class="img-responsive" src="img/17.jpg" alt="">
+                    <h3>Naman Gupta<br>
+                        <small>PUBLIC RELATIONS MANAGER AND UI DEVELOPER</small>
+                    </h3>
+                </div>
+                <div class="col-sm-2 text-center">
+                    <img class="img-responsive" src="img/13.jpg" alt="">
+                    <h3>Aameen <br>
+                        <small>WEB DEVELOPER</small>
+                    </h3>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+</div>
+    
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; EV EUTOPIA 2020</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
