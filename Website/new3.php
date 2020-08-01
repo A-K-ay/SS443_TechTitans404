79607865713ff133e0953fe353aa2a645a110254
= <=>
2020-08-01 14:55:36 +0530

    other segment vehicle sections

---
+++ Website/new3.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>EV Eutopia</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/theme-casual1.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand"><img src="img/6.png" width="270" alt=""></div>
+    <div class="address-bar">The Perfect World Of Electric Vehicles.</div>
+   
+    <!-- Navigation -->
+   <?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+			<div class="col-lg-3">
+			<div class="brand"><img src="img/6b.png" width="270" alt=""></div>
+<br><br><br>
+
+        <h1 class="my-4">Categories</h1>
+        <div class="list-group">
+          <a href="new3.php" class="list-group-item">2 Wheeler</a>
+          <a href="new2.php" class="list-group-item">3 Wheeler</a>
+          <a href="new.php" class="list-group-item">Passenger car</a>
+        </div>
+
+      </div>
+	  <div class="col-lg-9">
+	  
+                <div class="col-lg-12 text-center">
+                    <div id="carousel-example-generic" class="carousel slide">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="img-responsive img-full" src="img/e1.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="img/e2.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="img/ee.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="icon-prev"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="icon-next"></span>
+                        </a>
+                    </div>
+					
+					</div>
+        </div>
+		</div>
+		</div>
+					
+				<div class="row">
+            <div class="box">
+				<div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">EV EUTOPIA
+                        <strong>2 Wheeler's</strong>
+                    </h2>
+                    <hr>
+                </div>
+                    
+					<div class="col-lg-12 text-center">
+                    <a href="img/e1.jpg"><div class="col-lg-6"><img class="img-responsive img-border img-full" src="img/e1.jpg" alt=""></div></a>
+                     <div class="col-lg-6"><h2>ELECTRIC VEHICLE<br> 2W-1
+                        <br>
+                        <small>May 13, 2020</small>
+                    </h2>
+                    <p>2W-1 Electric Mobility Electric Scooter is an Electric Scooter. 2W-1 Electric Mobility Electric Scooter gets Disc brakes in the front and rear. The kerb weight of Electric Scooter is 65 kg. 2W-1 Electric Mobility Electric Scooter has Tubeless Tyre and Alloy Wheels.</p>
+                    <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#mymodal">Read more</button>
+                    </div>
+					<hr/>
+                </div>
+				
+				<div class="col-lg-12">
+				<hr>
+				</div>
+				
+				<div class="col-lg-12 text-center">
+                    <a href="img/e2.jpg"><div class="col-lg-6"><img class="img-responsive img-border img-full" src="img/e2.jpg" alt=""></div></a>
+                     <div class="col-lg-6"><h2>ELECTRIC VEHICLE<br> 2W-2
+                        <br>
+                        <small>April 13, 2020</small>
+                    </h2>
+                    <p>LightSpeed builds powerful, reliable electric bicycles that are masterfully designed to attract appreciative glances as you ride along.Every LightSpeed Electric Bicycle comes with a Portable, Rechargeable Lithium Ion Battery, a Powerful 250W Rear Hub Motor, a Lightweight Aluminium Alloy Frame, Multi-Level Pedal Boost, and an Agile Twist Throttle.</p>
+                    <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#mymodal1">Read more</button>
+                    </div>
+					<hr/>
+                </div>
+				
+				<div class="col-lg-12">
+				<hr>
+				</div>
+				
+				
+				<div class="col-lg-12 text-center">
+                    <a href="img/ee.jpg"><div class="col-lg-6"><img class="img-responsive img-border img-full" src="img/ee.jpg" alt=""></div></a>
+                     <div class="col-lg-6"><h2>ELECTRIC<br> 2W-3
+                        <br>
+                        <small>December 13, 2019</small>
+                    </h2>
+                    <p>EV Eutopia has made it a passionate calling to move you - simply, efficiently and intelligently. The EV Eutopia Personal Transporter (PT) is the first two-wheel self-balancing electric vehicle to utilise patented dynamic stabilising technology. Today, we at EV Eutopia are focused on offering our clients a safe, unique product that newly defines personal movement for consumers and business clients.</p>
+                    <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#mymodal2">Read more</button>
+                    </div>
+					<hr/>
+                </div>
+				
+				<div class="col-lg-12">
+				<hr>
+				</div>
+				
+				
+				
+				
+					
+				
+            
+	
+
+       <div id="mymodal" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+		  <div class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal">&times;</button> 
+		    <h4 class="modal-title">electrical vehicle 2W-1</h4>
+		  </div>
+		  <div class="modal-body">
+		    <p><h3>Features</h3>
+			<ul>
+			  <li><h4>With TVS iQube Scooter you enjoy a delightful, smooth, silent riding experience with instant torque. Intuitive hi-tech features like SmartXonnect and Q-Park Assist make it an unmatched intelligent experience.</h4></li>
+			  <li><h4>TVS iQube Scooter ensures you have a hassle-free ownership experience, 60 mins charge provides 15–20 km range, a 3-year warranty on the advanced Li-ion battery and access to a responsive customer care team.</h4></li>
+			  <li><h4>The new TVS iQube is designed to reflect your contemporary lifestyle, with its cutting edge technology, and sharp lines that come together to give incomparable comfort and convenience to your ride.</h4></li>
+			  <li><h4>Meet your practical need for everyday performance with more enjoyable and efficient rides, without adding any noise to the city!</h4></li>
+			
+			</ul>
+			</p>
+			<p><h3>Specifications</h3>
+			<ul>
+			  <li><h4>Weight - 118 kg</h4></li>
+			  <li><h4>Suspension - Front - Telescopic<br>Rear - Hydraulic twin tube shock absorber</h4></li>
+			  <li><h4>Battery Type - Voltage	Lithium-ion, 48V</h4></li>
+			  <li><h4> Tyre Size - Front - 90/90-12, Tubeless</h4></li>
+			  <li><h4>Warranty - 36 months*</h4></li>
+			
+			</ul>
+			</p>
+			<h3>Price starts from:<h4>Rs 1,10,000 </h4></h3>
+		  </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+		  
+        </div>
+      </div>
+    </div>	
+    <div id="mymodal1" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+		  <div class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal">&times;</button> 
+		    <h4 class="modal-title">electrical vehicle 2W-2</h4>
+		  </div>
+		  <div class="modal-body">
+		    <p><h3>Features</h3>
+			<ul>
+			  <li><h4>A backlit display that shows you a host of readings such as your trip distance, trip time, speed, motor temperature, and much more.It allows you to engage Cruise Control and Walk Assist at the touch of a button.</h4></li>
+			  <li><h4>The lightweight aluminium frame helps balance out the weight of the fat tyres, while lending the cyclist greater agility as he rides the fat electric cycle of his dreams.</h4></li>
+			  <li><h4>Rest your worries about the pick-up of your fat tyre cycle. The 250 Watt Rear Hub Motor and enhanced 45Nm Torque enable you to extract maximum power from your fat tyre bicycle from the very first pedal.</h4></li>
+			  <li><h4>When you buy a fat electric bicycle like the LightSpeed 2W-2, it comes with in-built 21 Speed Gears from Shimano. Not only do these gears give you greater choice with respect to the intensity of your ride, they also ease out slopes and mountains significantly.</h4></li>
+			   <li><h4> 2W-2 is an electric bicycle with fat tyres, it comes with a Portable 13.4Ah Lithium Ion Battery to give you an approximate range of 45 km per full charge, when you ride it at medium levels of Pedal Boost with a little bit of throttling.</h4></li>
+			
+			</ul>
+			</p>
+			<p><h3>Specifications</h3>
+			<ul>
+			  <li><h4>Powerful 250 Watt Brushless Rear Hub Motor</h4></li>
+			  <li><h4>Rechargable Lithium ion Battery</h4></li>
+			  <li><h4>Battery Type - 18650 series battery cells</h4></li>
+			  <li><h4> e-Brakes for Responsive Electric Cut-off</h4></li>
+			  <li><h4>Multi level Pedal Boost Technology</h4></li>
+			
+			</ul>
+			</p>
+			<h3>Price starts from:<h4>Rs 50,000</h4></h3>
+		  </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+		  
+        </div>
+      </div>
+    </div>	
+	<div id="mymodal2" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+		  <div class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal">&times;</button> 
+		    <h4 class="modal-title">electrical vehicle 2W-3 </h4>
+		  </div>
+		  <div class="modal-body">
+		    <p><h3>Features</h3>
+			<ul>
+			  <li><h4>UL 2272 certification ensures 2W-3 meets high standards for fire and electrical safety. The Smart Battery Management System provides reliable battery performance. IP54 waterproof protection ensures operation in various weathers.</h4></li>
+			  <li><h4>Sturdy & Powerful : 2W-3 is compact, weighting just 28 lbs with a max load of 220 lbs. Thanks to the dual 400W motors, it can easily reach a max speed of 10 mph. A single fully charged battery can run up to 13.7 miles, climbing a max slope of 15°</h4></li>
+			  <li><h4>Road Adaptive Design : 10.5" pneumatic tires provide a comfortable and smoother ride on bumpy roads. Knee control bar allows for precise steering and easy lifting. Applicable Age 16 – 50 years. Applicable Height - 3.11 – 6.6 Inch.</h4></li>
+			  <li><h4>Intelligent App Management : Download the Segway app for anti-theft function, speed limit and adjustment, vehicle self-diagnose, firmware upgrade, Ambient Light, vehicle remote control and more.</h4></li>
+			
+			</ul>
+			</p>
+			<p><h3>Specifications</h3>
+			<ul>
+			  <li><h4>Product Dimensions - 21.6 x 10.2 x 23.4 inches ; 28 pounds</h4></li>
+			  <li><h4>Max. Motor Powers - 1600 watts</h4></li>
+			  <li><h4>Max. Payload - 220 lbs</h4></li>
+			  <li><h4> Max. Climbing Slope - 15 degree</h4></li>
+			  <li><h4>Max. Speed - 10 mph</h4></li>
+			
+			</ul>
+			</p>
+			<h3>Price starts from:<h4>$490</h4></h3>
+		  </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+		  
+        </div>
+      </div>
+    </div>
+	</div>
+        </div>
+		</div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; EV EUTOPIA 2020</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
