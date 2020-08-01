c19ef9ff0efac8e809c31dd8ec5c88958f44a82b
Fwaad Ahmad <fwaad.ahmad2@gmail.com>
2020-08-01 09:53:07 +0530

    Initial Commit - Designed main activity

--- EV_Eutopia/app/src/androidTest/java/com/example/ev_eutopia/ExampleInstrumentedTest.java
+++ EV_Eutopia/app/src/androidTest/java/com/example/ev_eutopia/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.ev_eutopia;
+package com.example.eveutopia;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.ev_eutopia", appContext.getPackageName());
+        assertEquals("com.example.eveutopia", appContext.getPackageName());
     }
 }
