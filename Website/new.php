b34fcc074001d1e32567c43cb52cea4a7e3bb8bf
= <=>
2020-08-02 12:47:57 +0530

    changes made to registration and product section

---
+++ Website/new.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>EV Eutopia</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/theme-casual1.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand"><img src="img/6.png" width="270" alt=""></div>
+    <div class="address-bar">The Perfect World Of Electric Vehicles.</div>
+   
+    <!-- Navigation -->
+   <?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+			<div class="col-lg-3" >
+			<div class="brand"><img src="img/6b.png" width="270" alt=""></div>
+<br><br><br>
+        <h1 class="my-4" align="text-center" >Categories</h1>
+        <div class="list-group">
+          <a href="new3.php" class="list-group-item">2 Wheeler</a>
+          <a href="new2.php" class="list-group-item">3 Wheeler</a>
+          <a href="new.php" class="list-group-item">Passenger car</a>
+		  <a href="homechargers.php" class="list-group-item">Portable Home chargers</a>
+        </div>
+
+      </div>
+	  <div class="col-lg-9">
+	  
+                <div class="col-lg-12 text-center">
+                    <div id="carousel-example-generic" class="carousel slide">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="img-responsive img-full" src="img/e7.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="img/e9.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="img/e8.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="icon-prev"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="icon-next"></span>
+                        </a>
+                    </div>
+					
+					</div>
+        </div>
+		</div>
+		</div>
+	
+					<div class="row">
+            <div class="box">
+				
+				<div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">EV EUTOPIA
+                        <strong>Vehicles</strong>
+                    </h2>
+                    <hr>
+                </div>
+                    
+					<div class="col-lg-12 text-center">
+                   <a href="teslas.php"> <div class="col-lg-6"><img class="img-responsive img-border img-full" src="img/e7.jpg" alt=""></div></a>
+                     <div class="col-lg-6"><h2>ELECTRIC VEHICLE<br> Model S
+                        <br>
+                        <small>March 13, 2020</small>
+                    </h2>
+                    <p>Model S is built with best in class storage, seating for up to five adults and an expansive 17-inch touchscreen. Advanced noise engineering creates sound dynamics comparable to a recording studio, while the standard Glass Roof provides a spacious interior experience for every passenger.Model S is built with best in class storage, seating for up to five adults and an expansive 17-inch touchscreen. </p>
+                    <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#mymodal">Read more</button>
+                    </div>
+					<hr/>
+                </div>
+				
+				<div class="col-lg-12">
+				<hr>
+				</div>
+				
+				<div class="col-lg-12 text-center">
+                   <a href="zsev.php"> <div class="col-lg-6"><img class="img-responsive img-border img-full" src="img/e9.jpg" alt=""></div></a>
+                     <div class="col-lg-6"><h2>ELECTRIC VEHICLE <br> ZS EV
+                        <br>
+                        <small>January 13, 2020</small>
+                    </h2>
+                    <p>MG ZS EV is designed for those who want all the advantages of a zero emission car without compromising on style.MG ZS EV is designed for those who want all the advantages of a zero emission car without compromising on style</p>
+                    <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#mymodal1">Read more</button>
+                    </div>
+					<hr/>
+                </div>
+				
+				<div class="col-lg-12">
+				<hr>
+				</div>
+				
+				
+				<div class="col-lg-12 text-center">
+                    <a href="bolt.php"><div class="col-lg-6"><img class="img-responsive img-border img-full" src="img/e8.jpg" alt=""></div></a>
+                     <div class="col-lg-6"><h2>ELECTRIC VEHICLE<br> Bolt EV
+                        <br>
+                        <small>November 13, 2019</small>
+                    </h2>
+                    <p>From your weekday commute to weekend trips and all the places in between, Bolt EV is ready to take you wherever you want to go. This ride is an affordable all-electric vehicle that offers an EPA-estimated 259 miles of range on a full charge.† Plus, it offers advanced safety technologies, plenty of space and good looks on top of that. With Bolt EV, there’s nothing holding you back</p>
+                    <button type="button" class="btn btn-info btn-large" data-toggle="modal" data-target="#mymodal2">Read more</button>
+                    </div>
+					<hr/>
+                </div>
+				
+				<div class="col-lg-12">
+				<hr>
+				</div>
+				
+				
+				
+				
+					
+				
+            
+
+       <div id="mymodal" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+		  <div class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal">&times;</button> 
+		    <h2 class="modal-title">electrical vehicle Model S</h2>
+		  </div>
+		  <div class="modal-body">
+		    <p><h3>Features</h3>
+			<ul>
+			  <li><h4>Model S is built from the ground up as an electric vehicle, with high-strength architecture and a floor-mounted battery pack allowing for incredible impact protection.</h4></li>
+			  <li><h4>Model S sets an industry standard for performance and safety. Tesla’s all-electric powertrain delivers unparalleled performance in all weather conditions – with Dual Motor All-Wheel Drive, adaptive air suspension and ludicrous acceleration.</h4></li>
+			  <li><h4>Recharge up to 163 miles in 15 minutes at any Supercharger location.Go anywhere with up to 402 miles of range on a single charge</h4></li>
+			  <li><h4>Autopilot advanced safety and convenience features are designed to assist you with the most burdensome parts of driving.</h4></li>
+			  <li><h4>Model S is built with best in class storage, seating for up to five adults and an expansive 17-inch touchscreen. Advanced noise engineering creates sound dynamics comparable to a recording studio, while the standard Glass Roof provides a spacious interior experience for every passenger.</h4></li>
+			
+			</ul>
+			</p>
+			<p><h3>Specifications</h3>
+			<ul>
+			  <li><h4>Weight - 4,941 lbs</h4></li>
+			  <li><h4>Displays - Driver Display + 17" Touchscreen</h4></li>
+			  <li><h4>Supercharging - Max/Payment Type250 kW max; Pay Per Use</h4></li>
+			  <li><h4>Seating - 5 Adults</h4></li>
+			  <li><h4>Warranty Basic Vehicle - 4 years or 50,000 miles, whichever comes first<br>
+			  Battery & Drive Unit - 8 years or 150,000 miles, whichever comes first</h4></li>
+			
+			</ul>
+			</p>
+			<h3>Price starts from:<h4>$69,490 - $89,490</h4></h3>
+		  </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+		  
+        </div>
+      </div>
+    </div>
+	
+    <div id="mymodal1" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+		  <div class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal">&times;</button> 
+		    <h2 class="modal-title">electrical vehicle ZS EV</h2>
+		  </div>
+		  <div class="modal-body">
+		    <p><h3>Features</h3>
+			<ul>
+			  <li><h4>Inspired from various elements of the celestial bodies and Nature. MG ZS EV is all packed to fulfill your style quotient.</h4></li>
+			  <li><h4>Safety is our foundation at MG. The ZS EV comes with the 5 star NCAP rating.</h4></li>
+			  <li><h4>MG ZS EV is an Electric suv Car. The electric motor generates a power of 140.8bhp@3500rpm and a torque of 353Nm@5000rpm.</h4></li>
+			  <li><h4>MG ZS EV is available in Automatic transmission only.</h4> </li>
+			  <li><h4>In configurations, MG ZS EV has a dimensions of 4314 mm in length, 1809 mm in width and 1620 mm in height.</h4></li>
+			
+			</ul>
+			</p>
+			<p><h3>Specifications</h3>
+			<ul>
+			  <li><h4>Fuel Type - Electric</h4></li>
+			  <li><h4>Power - 140.8bhp@3500rpm</h4></li>
+			  <li><h4>Seating Capacity - 5</h4></li>
+			  <li><h4>Transmission - automatic</h4></li>
+			  <li><h4>Driving Modes - Eco, Sport & Normal</h4></li>
+			
+			</ul>
+			</p>
+			<h3>Price starts from:<h4>Rs. 20.88 - 23.58 Lakh</h4></h3>
+		  </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+		  
+        </div>
+      </div>
+    </div>	
+	<div id="mymodal2" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+		  <div class="modal-header">
+		    <button type="button" class="close" data-dismiss="modal">&times;</button> 
+		    <h2 class="modal-title">electrical vehicle Bolt EV </h2>
+		  </div>
+		  <div class="modal-body">
+		    <p><h3>Features</h3>
+			<ul>
+			  <li><h4>Bolt EV offers an EPA-estimated 259 miles of range,nearly 10% more than previous model years, or 21 miles farther than before. You can go a day – or possibly even a few days – without needing to charge. That means you’ll have all the range you need for your daily routine plus a few stops along the way.</h4></li>
+			  <li><h4>Ready to get going? The myChevrolet Mobile App with Energy Assist uses key information such as the vehicle’s current charge, driving conditions and real-time weather to calculate the most efficient route for you. If you need to stop for a charge, Energy Assist points you to the nearest charging station and automatically updates as conditions change, so you’ll know exactly where to go.</h4></li>
+			  <li><h4>Sometimes, the more information the better. The 10.2-inch diagonal color touch-screen display gives you tons of real-time vehicle information, such as your battery levels, estimated range and charge settings. Plus, split screens and expanded views keep your music, contacts and everything else within reach.</h4></li>
+			  <li><h4>Outside, the beautifully sculpted body and eye-catching details grab attention wherever you go. Inside, there’s comfortable seating for five with plenty of shoulder, head and leg room for everybody.</h4></li>
+			  <li><h4>It’s your space — use it how you want. If you’re loading large or odd-sized items, simply fold down the rear seats for 56.6 cu. ft.of maximum cargo room. And the rear area in this electric vehicle features a deep well with a false cargo floor (standard on Premier) for even more room to store your things.</h4></li>
+			
+			</ul>
+			</p>
+			<p><h3>Specifications</h3>
+			<ul>
+			  <li><h4>BATTERY - liquid-cooled lithium-ion, 66.0 kWh</h4></li>
+			  <li><h4>TRANSMISSION - single-speed direct drive</h4></li>
+			  <li><h4>Suspension (F/R): struts/multilink<br>
+			  Brakes (F/R) -  10.9-in vented disc/10.4-in disc</h4></li>
+			  <li><h4>VEHICLE TYPE - front-motor, front-wheel-drive, 5-passenger, 4-door hatchback</h4></li>
+			  <li><h4>EPA FUEL ECONOMY - Combined/city/highway: 118/127/108 MPGe<br>
+			  Range - 259 miles</h4></li>
+			
+			</ul>
+			</p>
+			<h3>Price starts from:<h4>$36,620 - $41,420</h4></h3>
+		  </div>
+          <div class="modal-footer">
+             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+		  
+        </div>
+      </div>
+    </div>
+	</div>
+        </div></div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; EV EUTOPIA 2020</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
